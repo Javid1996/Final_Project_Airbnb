d654dd8f9b4ec3f2bc37a67cc9217789a607f06e
Javid1996 <alcavid96@gmail.com>
2023-11-26 01:12:36 +0400

    backend

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,13 +1,25 @@
 import React from "react";
+import {useState} from "react";
 import '../Styles/Banner.css'
 import {Button} from "@mui/material" 
-function Banner(){
+import Search from "./Search";
 
+
+
+
+function Banner(){
+    const [showSearch, setShowSearch]=useState(true)
 
     return(
         <div className="banner">
+            <div className="banner__search">
+                {showSearch && <Search/>}
+                <Button onClick={()=>{setShowSearch(!showSearch)}} className="banner__searchButton" variant="outlined">Search Dates</Button>
+            </div>
             <div className="banner__info">
-                
+            <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getaway to uncover the hidden gems near you</h5>
+                <Button variant="outlined">Click & Go</Button>
             </div>
         </div>
     )
